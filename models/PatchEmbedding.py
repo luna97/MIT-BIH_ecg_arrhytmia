ec4a41a5c19418ad6a9a084ca6d8a8e956a6232f
luna97 <rikilnl@gmail.com>
2025-02-14 14:56:42 +0100

    lol

--- models/PatchEmbedding.py
+++ models/PatchEmbedding.py
@@ -7,6 +7,6 @@ class PatchEmbedding(nn.Module):
         self.conv = nn.LazyConv1d(num_hiddens, kernel_size=patch_size, stride=patch_size)
 
     def forward(self, X):
-        print('X shape', X.shape)
+        # print('X shape', X.shape)
         # Output shape: (batch size, no. of patches, no. of channels)
         return self.conv(X).flatten(2).transpose(1, 2)
